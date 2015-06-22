0d2d7fb36f0b2aecf264d234620223d8d158ef5c
Horacio Ayala <horacio_dev@pmovil.com.ar>
2015-06-22 17:16:00 -0300

    XX

--- app/src/main/java/com/lod/horaceayala/myapplication/Main2Activity.java
+++ app/src/main/java/com/lod/horaceayala/myapplication/Main2Activity.java
@@ -1,15 +1,28 @@
 package com.lod.horaceayala.myapplication;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends ActionBarActivity {
 
     private static final String NAV_ITEM_ID = "navItemId";
     private static final long DRAWER_CLOSE_DELAY_MS = 300;
@@ -18,80 +31,86 @@ public class Main2Activity extends AppCompatActivity {
     private int mNavItemId;
     private MainFragment mFirstFragment = new MainFragment();
     private FixtureFragment mFixtureFragment = new FixtureFragment();
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
+
+    NavigationAdapter navigationMenuAdapter;
+    NavigationItem[] navigationMenuItems = {
+            new NavigationItem(android.R.string.cancel, android.R.drawable.ic_delete, new MainFragment()),
+            new NavigationItem(android.R.string.copy, android.R.drawable.ic_delete, new MainFragment()),
+            new NavigationItem(android.R.string.cut, android.R.drawable.ic_dialog_alert, new MainFragment()),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
 
-//        toolbar.setNavigationIcon(android.R.drawable.ic_menu_more);
+        navigationMenuAdapter = new NavigationAdapter(this, navigationMenuItems);
+        mDrawerList.setAdapter(navigationMenuAdapter);
 
-        // load saved navigation state if present
-        if (null == savedInstanceState) {
-            mNavItemId = R.id.drawer_item_1;
-        } else {
-            mNavItemId = savedInstanceState.getInt(NAV_ITEM_ID);
-            mNavItemId = R.id.drawer_item_1;
-        }
+//        addDrawerItems();
+        setupDrawer();
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, android.R.string.copy, android.R.string.cancel) {
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                invalidateOptionsMenu();
-                syncState();
-            }
 
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                invalidateOptionsMenu();
-                syncState();
-            }
-        };
-        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        actionBarDrawerToggle.syncState();
-
-
-//        // listen for navigation events
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
-//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(final MenuItem menuItem) {
-//                // allow some time after closing the drawer before performing real navigation
-//                // so the user can see what is happening
-//                mDrawerLayout.closeDrawer(GravityCompat.START);
-//                Handler mDrawerActionHandler = new Handler();
-//                mDrawerActionHandler.postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        navigate(menuItem.getItemId());
-//                    }
-//                }, DRAWER_CLOSE_DELAY_MS);
-//                return true;
-//            }
-//        });
-
-        navigate(mNavItemId);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
-    private void navigate(int mNavItemId) {
-        switch (mNavItemId) {
-            case R.id.drawer_item_1:
-                getSupportFragmentManager().beginTransaction().remove(mFixtureFragment).commit();
-                getSupportFragmentManager().beginTransaction().add(R.id.content, mFirstFragment).addToBackStack(null).commit();
-                break;
-            case R.id.drawer_item_2:
-                getSupportFragmentManager().beginTransaction().remove(mFirstFragment).commit();
-                getSupportFragmentManager().beginTransaction().add(R.id.content, mFixtureFragment).addToBackStack(null).commit();
-                break;
-            default:
-                // ignore
-                break;
-        }
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Main2Activity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, android.R.string.cancel, android.R.string.copy) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -113,6 +132,86 @@ public class Main2Activity extends AppCompatActivity {
             return true;
         }
 
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public class NavigationAdapter extends BaseAdapter {
+        private Context context;
+        private NavigationItem[] navigationItems;
+
+        public NavigationAdapter(Context context, NavigationItem[] items) {
+            this.context = context;
+            navigationItems = items;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = null;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                row = inflater.inflate(R.layout.list_item_navigation, parent, false);
+            } else {
+                row = convertView;
+            }
+
+            TextView tvTitle = (TextView) row.findViewById(R.id.textView);
+            ImageView ivIcon = (ImageView) row.findViewById(R.id.imageView);
+
+            tvTitle.setText(navigationItems[position].stringTitle);
+            ivIcon.setImageResource(navigationItems[position].drawableIcon);
+            return row;
+        }
+    }
+
+    private class NavigationItem
+    {
+        /**
+         *
+         * @param stringTitle The id of the string resource of the text of the item.
+         * @param drawableIcon The id of the drawable resource of icon of the item.
+         * @param fragment The Fragment to be loaded upon selecting the item.
+         */
+        public NavigationItem(int stringTitle, int drawableIcon, Fragment fragment)
+        {
+            this.stringTitle = stringTitle;
+            this.drawableIcon = drawableIcon;
+            this.fragment = fragment;
+        }
+
+        /**
+         * The id of the string resource of the text of the item.
+         */
+        public int stringTitle;
+
+        /**
+         * The id of the drawable resource of icon of the item.
+         */
+        public int drawableIcon;
+
+        /**
+         * The Fragment to be loaded upon selecting the item.
+         */
+        public Fragment fragment;
     }
 }
